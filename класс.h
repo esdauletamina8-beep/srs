ad55c11438912f47c10edbf281a27c6e576bc60b
esdauletamina8-beep <esdauletamina8@gmail.com>
2026-09-25 22:27:03 +0500

    Update класс.h

--- класс.h
+++ класс.h
@@ -1,1 +1,31 @@
-hg
+#include <iostream>
+#include <string>
+using namespace std;
+
+class BankAccount {
+private:
+    string name;
+    string accountNumber;
+    double balance;
+
+public:
+    BankAccount(string n, string num, double b) {
+        name = n;
+        accountNumber = num;
+        balance = b;
+    }
+
+    void showInfo() {
+        cout << "Owner: " << name << endl;
+        cout << "Account number: " << accountNumber << endl;
+        cout << "Balance: " << balance << endl;
+    }
+};
+
+int main() {
+    BankAccount account("Amina", "KZ123456", 10000);
+
+    account.showInfo();
+
+    return 0;
+}
